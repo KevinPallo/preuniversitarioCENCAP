39c95662adec1925b67dd16e49ab64a6d411f3de
Kevin Pallo <kevin-akd1@hotmail.com>
2022-09-09 15:41:27 -0500

    Inicio ready

--- frontend/src/app/pages/login/login.component.ts
+++ frontend/src/app/pages/login/login.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute, Params } from '@angular/router';
 
 @Component({
   selector: 'app-login',
@@ -7,9 +8,16 @@ import { Component, OnInit } from '@angular/core';
 })
 export class LoginComponent implements OnInit {
 
-  constructor() { }
+  typeLogin:{parametro:string}
+
+  constructor(private rutaActiva: ActivatedRoute) {
+    this.typeLogin = {
+      parametro:this.rutaActiva.snapshot.params['parametro']
+    }
+   }
 
   ngOnInit(): void {
+    
   }
 
 }
